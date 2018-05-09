2aeaf1f2dccd8a6c3be447d81fd81a00c05e0608
babestvl <babestvl@gmail.com>
2018-05-09 15:45:26 +0700

    Remove unused import

--- screens/MetricScreen.js
+++ screens/MetricScreen.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Text, View, Button } from 'react-native';
 import firebase from 'firebase';
 import styles from '../styles';
--- screens/StackScreen.js
+++ screens/StackScreen.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { View, Text } from 'react-native';
 import { StackNavigator } from 'react-navigation';
 import DataScreen from './DataScreen';
